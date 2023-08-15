fa63d4f39e2b7af371d07b99b43d79e0f2952ac8
Shyamthakur5487 <142294721+Shyamthakur5487@users.noreply.github.com>
2023-08-15 19:55:26 +0530

    Add files via upload
    add them all to src section in react script

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AppGame from "./components/game/AppGame.jsx"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+        <Route path="/app" element={<AppGame />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
